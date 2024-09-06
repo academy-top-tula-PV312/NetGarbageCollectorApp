69305b77ce482347b66211b5247f9a4b046a88e2
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-09-06 21:17:19 +0300

    Добавьте файлы проекта.

---
+++ NetGarbageCollectorApp/Program.cs
@@ -0,0 +1,23 @@
+﻿Employee e = null;
+
+Console.WriteLine(System.GC.GetTotalMemory(false));
+
+for (int i = 0; i < 100; i++)
+     e = new Employee();
+
+Console.WriteLine(System.GC.GetTotalMemory(false));
+Console.WriteLine($"{System.GC.GetGeneration(e)}");
+
+System.GC.Collect();
+
+Console.WriteLine(System.GC.GetTotalMemory(false));
+Console.WriteLine($"{System.GC.GetGeneration(e)}");
+
+
+
+class Employee
+{
+    public string Name { set; get; }
+    public int Age { set; get; }
+    int[] array = new int[1000];
+}
