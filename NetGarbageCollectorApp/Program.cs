fc765de9e82b614f387dc986a04dbddcb470b165
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-09-09 19:09:52 +0300

    09-09-24

--- NetGarbageCollectorApp/Program.cs
+++ NetGarbageCollectorApp/Program.cs
@@ -1,23 +1,61 @@
-﻿Employee e = null;
+﻿//Employee e = null;
 
-Console.WriteLine(System.GC.GetTotalMemory(false));
+//Console.WriteLine(System.GC.GetTotalMemory(false));
 
-for (int i = 0; i < 100; i++)
-     e = new Employee();
+//for (int i = 0; i < 100; i++)
+//     e = new Employee();
 
-Console.WriteLine(System.GC.GetTotalMemory(false));
-Console.WriteLine($"{System.GC.GetGeneration(e)}");
+//Console.WriteLine(System.GC.GetTotalMemory(false));
+//Console.WriteLine($"{System.GC.GetGeneration(e)}");
 
-System.GC.Collect();
+//System.GC.Collect();
 
-Console.WriteLine(System.GC.GetTotalMemory(false));
-Console.WriteLine($"{System.GC.GetGeneration(e)}");
+//Console.WriteLine(System.GC.GetTotalMemory(false));
+//Console.WriteLine($"{System.GC.GetGeneration(e)}");
+
+
+//Test();
+
+//Console.WriteLine("Pressw any key");
+//Console.ReadKey();
+
+unsafe
+{
+    const int size = 10;
+    int* array = stackalloc int[size];
+}
 
 
 
-class Employee
+
+void Test()
+{
+    //Employee employee = null;
+    //try
+    //{
+    //    employee = new Employee() { Name = "Bobby" };
+    //}
+    //finally
+    //{
+    //    employee.Dispose();
+    //}
+
+    using(Employee employee = new Employee() { Name = "Bobby" })
+    {
+
+    }
+}
+
+
+
+class Employee : IDisposable
 {
     public string Name { set; get; }
     public int Age { set; get; }
-    int[] array = new int[1000];
+    //int[] array = new int[1000];
+
+    public void Dispose()
+    {
+        Console.WriteLine($"Employee {Name} deleted");
+    }
 }
